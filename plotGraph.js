652e1c8381d374e235e154f025a53b59c73396c3
genka2015 <yeepeekaye@yandex.ru>
2015-11-23 15:41:23 -0500

    Update plotGraph.js
    added a bunch of stuff for line and bar. didn't add stream parameters yet

--- plotGraph.js
+++ plotGraph.js
@@ -1,5 +1,4 @@
 
-// Stays the same
 
 var graph = {
   // General configuration options
@@ -9,7 +8,8 @@ var graph = {
 
     backgroundColor: "#FFFFFF",
     textColor: "#ccc",
-
+	//showlegend: true,
+	
     domains: {
       x: {
         // The color of the axis
@@ -55,31 +55,211 @@ var graph = {
   datapoints: {
     groups: [
       {
+		// Name for this group
+        name:        "gcc",
+		
+		// Type for group  
+		type: 		 "scatter",
+		
         // Color for this group
         color:       "hsl(120, 60%, 60%)",
 
-        // Name for this group
-        name:        "gcc",
+		// Visible 	true  |  false  |  "legendonly"
+		visible:	 true,
+		
+		// Boolean
+		showlegend:	 true,
+		
+		// Legend group for the legend
+		legendgroup: "",
+		
+		// Opacity of the element	0-1
+		opacity: 1,
+		
+		// Text to display
+		text: "",
+		
+		//  any combination of "lines" "markers" "text" joined with "+" or "none"
+		mode: "",
+		
+		//	line
+		line:{
+			width: 3,
+			//	"linear" | "spline" | "hv" | "vh" | "hvh" | "vhv"
+			shape: "spline",
+			//  only for "spline", 0-1.3
+			smoothing: "1",
+			//  String, default "solid", set to dash type or pixel length
+			dash: "5",
+			//	Boolean; if gaps ({nan} or missing values) in data arrays are connected
+			connectgaps: true,
+			//	markers
+			marker:{
+				symbol: "0",
+				opacity: 1,
+				size: 8,
+				color: "",
+			},
+		},
+		
+		//  bar
+		bar:{
+			// "h" | "v"  orientation of the bars
+			orientation: "v",
+			marker:{
+				color: "",
+			},
+		}
       },
       {
+		name:        "clang clang clang",
+		type:		 "scatter",
         color:       "hsl(0, 60%, 60%)",
-        name:        "clang clang clang",
+		visible:	 true,
+		showlegend:	 true,
+		legendgroup: "",
+		opacity: 1,
+		text: "",
+		mode: "",
+		line:{
+			width: 3,
+			shape: "spline",
+			smoothing: "1",
+			dash: "solid",
+			connectgaps: true,
+			marker:{
+				symbol: "0",
+				opacity: 1,
+				size: 8,
+				color: "",
+			},
+		},
+		bar:{
+			orientation: "v",
+			marker:{
+				color: "",
+			},
+		}
       },
       {
+		name:        "msc",
+		type:		 "scatter",
         color:       "hsl(180, 60%, 60%)",
-        name:        "msc",
+		visible:	 true,
+		showlegend:	 true,
+		legendgroup: "",
+		opacity: 1,
+		text: "",
+		mode: "",
+		line:{
+			width: 3,
+			shape: "spline",
+			smoothing: "1",
+			dash: "solid",
+			connectgaps: true,
+			marker:{
+				symbol: "0",
+				opacity: 1,
+				size: 8,
+				color: "",
+			},
+		},
+		bar:{
+			orientation: "v",
+			marker:{
+				color: "",
+			},
+		}
       },
       {
+		name:        "new",
+		type:		 "scatter",
         color:       "hsl(30, 60%, 60%)",
-        name:        "new",
+		visible:	 true,
+		showlegend:	 true,
+		legendgroup: "",
+		opacity: 1,
+		text: "",
+		mode: "",
+		line:{
+			width: 3,
+			shape: "spline",
+			smoothing: "1",
+			dash: "solid",
+			connectgaps: true,
+			marker:{
+				symbol: "0",
+				opacity: 1,
+				size: 8,
+				color: "",
+			},
+		},
+		bar:{
+			orientation: "v",
+			marker:{
+				color: "",
+			},
+		}
       },
       {
+		name:        "smth",
+		type:		 "scatter",
         color:       "hsl(80, 60%, 60%)",
-        name:        "smth",
+		visible:	 true,
+		showlegend:	 true,
+		legendgroup: "",
+		opacity: 1,
+		text: "",
+		mode: "",
+		line:{
+			width: 3,
+			shape: "spline",
+			smoothing: "1",
+			dash: "solid",
+			connectgaps: true,
+			marker:{
+				symbol: "0",
+				opacity: 0.5,
+				size: 8,
+				color: "",
+			},
+		},
+		bar:{
+			orientation: "v",
+			marker:{
+				color: "",
+			},
+		}
       },
       {
+		name:        "here",
+		type:		 "scatter",
         color:       "hsl(50, 60%, 60%)",
-        name:        "here",
+		visible:	 true,
+		showlegend:	 true,
+		legendgroup: "",
+		opacity: 1,
+		text: "huehue",
+		mode: "lines+text",
+		line:{
+			width: 6,
+			shape: "spline",
+			smoothing: "1",
+			dash: "solid",
+			connectgaps: true,
+			marker:{
+				symbol: "0",
+				opacity: 1,
+				size: 8,
+				color: "",
+			},
+		},
+		bar:{
+			orientation: "v",
+			marker:{
+				color: "",
+			},
+		}
       }
     ]
   }
@@ -89,17 +269,17 @@ var graph = {
 var data = {
   datapoints: {
     groups: [{
-      series: [80, 100, 90,75]
+      series: [20, 40, 90,75]
     }, {
-      series: [73, 80,  76,70]
+      series: [73, 32,  76,70]
     }, {
-      series: [93, 84,  64,86]
+      series: [13, 64,  64,26]
     }, {
-      series: [82, 92,  70,78]
+      series: [22, 52,  70,18]
     }, {
-      series: [74, 86,  90,100]
+      series: [34, 36,  50,10]
     }, {
-      series: [72, 61,  89,74]
+      series: [60, 31,  49,60]
     }]
   }
 };
@@ -116,6 +296,14 @@ function setData(description, data){
   var m = data.length;		// 6 data objects to make
   var xLabels = configuration.domains.x.series || [];
   var legend_labels = groups.map(function(d) { return d.name; });
+  var line_colors = groups.map(function(d) { return d.color; });
+  var type = groups.map(function(d) { return d.type});
+  var visible = groups.map(function(d) { return d.visible});
+  var showlegend = groups.map(function(d) { return d.showlegend});
+  var legendgroup = groups.map(function(d) { return d.legendgroup});
+  var opacity = groups.map(function(d) { return d.opacity});
+  var text = groups.map(function(d) { return d.text});
+  var mode = groups.map(function(d) { return d.mode});
   
   if (xLabels === undefined) {
     xLabels = [];
@@ -133,18 +321,100 @@ function setData(description, data){
   
   for(var i=0;i<m;i++){
 	  var dataElement = {};
+	  
 	  dataElement.x = x;						// Assigning X values to one data object
 	  dataElement.y = data[i].series;			// Assigning Y values
-	  dataElement.type = "scatter";				// "scatter" - line; "bar" - bar
-	  dataElement.name = legend_labels[i];		//  line and legend name
+	  dataElement.type = type[i];				// "scatter" - line; "bar" - bar
+	  dataElement.name = legend_labels[i];	//  line and legend name
+	  dataElement.visible = visible[i];			//	true  |  false  |  "legendonly"
+	  dataElement.showlegend = showlegend[i];		//  Boolean
+	  dataElement.legendgroup = legendgroup[i];			//  String
+	  dataElement.opacity = opacity[i];				//	0-1
+	  dataElement.stream = {};
+	  dataElement.stream.token = "id";		//  id number links data trace with stream
+	  dataElement.stream.maxpoints = 50;	//	max points to display
+	  dataElement.text = text[i];				//	text to display on data element
+	  dataElement.mode = mode[i];				//  any combination of "lines" "markers" "text" joined with "+" or "none"
 	  //------line options ----------------
-	  dataElement.line = {};
-	  //dataElement.line.color = "blue";		// line color
-	  dataElement.line.shape = "spline";		//	"linear" | "spline" | "hv" | "vh" | "hvh" | "vhv"
-	  
+	  if(type[i] == "scatter" || !type){
+		dataElement.line = {};
+		dataElement.line.color = line_colors[i];		// line color
+		dataElement.line.width = groups[i].line.width;
+		dataElement.line.shape = groups[i].line.shape;		//	"linear" | "spline" | "hv" | "vh" | "hvh" | "vhv"
+		dataElement.line.smoothing = groups[i].line.smoothing;		//  only for "spline", 0-1.3
+		dataElement.line.dash = groups[i].line.dash;			//  String, default "solid", set to dash type or pixel length
+		dataElement.connectgaps = groups[i].line.connectgaps;		//	Boolean; if gaps ({nan} or missing values) in data arrays are connected
+		
+		dataElement.marker = {};
+		dataElement.marker.symbol = groups[i].line.marker.symbol;
+		dataElement.marker.opacity = groups[i].line.marker.opacity;
+		dataElement.marker.size = groups[i].line.marker.size;
+		dataElement.marker.color = groups[i].line.marker.color;
+	  }
 	  //--------bar options ---------------
-	  //dataElement.orientation = "h";			// "h" | "v"  orientation of the bars
-	  
+	  if(type[i] == "bar"){
+		dataElement.orientation = groups[i].bar.orientation;			// "h" | "v"  orientation of the bars
+		dataElement.marker = {};
+		dataElement.marker.color = groups[i].bar.marker.color;
+	  }
+	  if(type == "heatmap"){
+		var z = [2];
+		var test = [20,40,75,90];
+		var test2 = [50,65,85,100];
+		z[0] = test;
+		z[1] = test2;
+		dataElement.z = z;
+	  }
+	  if(type == "histogram"){
+		
+	  }
+	  if(type == "histogram2d"){
+		
+	  }
+	  if(type == "pie"){
+		
+	  }
+	  if(type == "contour"){
+		var z = [2];
+		var test = [20,40,75,90];
+		var test2 = [50,65,85,100];
+		z[0] = test;
+		z[1] = test2;
+		dataElement.z = z;
+	  }
+	  if(type == "histogram2dcontour"){
+		
+	  }
+	  if(type == "scatter3d"){
+		dataElement.x = [35,44,57,70];
+		var z = [10,25,50,75];
+		//var test = [10,25,50,75];
+		//var test2 = [50,65,85,100];
+		//z[0] = test;
+		//z[1] = test2;
+		dataElement.z = z;
+	  }
+	  if(type == "surface"){
+		
+	  }
+	  if(type == "mesh3d"){
+		
+	  }
+	  if(type == "scattergeo"){
+		
+	  }
+	  if(type == "choropleth"){
+		
+	  }
+	  if(type == "scattergl"){
+		
+	  }
+	  if(type == "area"){
+		
+	  }
+	  if(type == "layout"){
+		
+	  }
 	  dataArray.push(dataElement);	// Adding data object to the array
   }
   return dataArray;
@@ -188,8 +458,6 @@ function setLayout(description, data){
   
   return layout;
 }
-
-//  Plot the graph with Plotly
 
 var myGraph = document.getElementById('myGraph');
 var plotData = []; 
